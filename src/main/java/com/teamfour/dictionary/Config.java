4959d1b25afa517f58de04a6ddf24bd9b1cd9593
vehbican <vehbican35@windowslive.com>
2023-04-28 19:10:36 +0300

    TEI files uploaded

--- src/main/java/com/teamfour/dictionary/Config.java
+++ src/main/java/com/teamfour/dictionary/Config.java
@@ -20,6 +20,55 @@ public class Config {
     public static final String eng_swe_tei = "dicts/eng-swe.tei";
     public static final String eng_fra_tei = "dicts/eng-fra.tei";
 
+
+    public static final String deu_tur_tei = "dicts/deu-tur-tei";
+    public static final String deu_ita_tei = "dicts/deu-ita-tei";
+    public static final String deu_fra_tei = "dicts/deu-fra-tei";
+    public static final String deu_ell_tei = "dicts/deu-ell-tei";
+    public static final String deu_eng_tei = "dicts/deu-eng-tei";
+    public static final String deu_swe_tei = "dicts/deu-swe-tei";
+
+
+
+    public static final String fra_eng_tei = "dicts/fra-eng.tei";
+    public static final String fra_ita_tei = "dicts/fra-ita.tei";
+    public static final String fra_deu_tei = "dicts/fra-deu.tei";
+    public static final String fra_tur_tei = "dicts/fra-tur.tei";
+    public static final String fra_ell_tei = "dicts/fra-ell.tei";
+    public static final String fra_swe_tei = "dicts/fra-swe.tei";
+
+
+    public static final String swe_eng_tei = "dicts/swe-eng.tei";
+    public static final String swe_tur_tei = "dicts/swe-tur.tei";
+    public static final String swe_fra_tei = "dicts/swe-fra.tei";
+    public static final String swe_ell_tei = "dicts/swe-ell.tei";
+    public static final String swe_deu_tei = "dicts/swe-deu.tei";
+    public static final String swe_ita_tei = "dicts/swe-ita.tei";
+
+
+    public static final String ita_eng_tei = "dicts/ita-eng.tei";
+    public static final String ita_deu_tei = "dicts/ita-deu.tei";
+    public static final String ita_ell_tei = "dicts/ita-ell.tei";
+    public static final String ita_swe_tei = "dicts/ita-swe.tei";
+    public static final String ita_tur_tei = "dicts/ita-tur.tei";
+    public static final String ita_fra_tei = "dicts/ita-fra.tei";
+
+
+
+
+/*
+    public static final String tur_deu_tei = "dicts/tur-deu-tei";
+    public static final String tur_eng_tei = "dicts/tur-eng.tei";*/
+
+
+
+
+
+
+
+
+
+
     public static int eng_tur_index = 0;
     public static int eng_fra_index = 1;
     public static int eng_ita_index = 2;
